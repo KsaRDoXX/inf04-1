fd967cece5cae7d76b49700b42c2066fff595114
WiktorKwiecien <80287210+WiktorKwiecien@users.noreply.github.com>
2022-09-07 21:44:06 +0200

    Add files via upload

---
+++ ArrayManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INF04_Aplikacja_Konsolowa___przeszukiwanie_tablicy_z_wartownikiem
+{
+    static public class ArrayManager
+    {
+        public static void RandWypelnianieTablicy(int[] tablica)
+        {
+            var rand = new Random();
+
+            for (int i = 0; i < tablica.Length; i++)
+            {
+                tablica[i] = rand.Next(101);
+            }
+        }
+        public static int WyszukajWartosc(int[] tablica, int wartosc)
+        {
+            tablica[tablica.Length - 1] = wartosc + 1;
+            int index;
+
+            for (int i = 0; i < tablica.Length; i++)
+            {
+                if (tablica[i] == wartosc)
+                {
+                    index = i;
+                    return index;
+                }
+            }
+
+            return tablica.Length;
+        }
+        public static void WypiszTablice(int[] tablica)
+        {
+            Console.WriteLine("Zawartość tablicy :\n ");
+            foreach (var item in tablica)
+            {
+                Console.Write(item + ", ");
+            }
+            Console.WriteLine("\n");
+        }
+        
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿
+using INF04_Aplikacja_Konsolowa___przeszukiwanie_tablicy_z_wartownikiem;
+using System.Runtime.CompilerServices;
+
+int[] tablica = new int[50];
+
+ArrayManager.RandWypelnianieTablicy(tablica);
+ArrayManager.WypiszTablice(tablica);
+
+Console.WriteLine("Wpisz wartość, którą chcesz wyszukać w tablicy :");
+
+int wartoscDoZnalezienia = 0;
+
+bool success = int.TryParse(Console.ReadLine(), out wartoscDoZnalezienia);
+
+if(success)
+{
+    int index = ArrayManager.WyszukajWartosc(tablica, wartoscDoZnalezienia);
+
+    if(index != tablica.Length)
+    {
+        ArrayManager.WypiszTablice(tablica);
+        Console.WriteLine("\n---\n");
+        Console.WriteLine("Szukana wartość jest pod indeksem :" + index);
+    } else
+    {
+        Console.WriteLine("W tablicy nie było szukanej wartości");
+    }
+    
+}else
+{
+    Console.WriteLine("Coś poszło nie tak");
+}
+
+
+
